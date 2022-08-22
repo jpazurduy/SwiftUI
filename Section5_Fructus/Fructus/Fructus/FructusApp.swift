57883ac7029ccae2cebaa1fdcbb1e7512d4d98ff
Jorge Azurduy <jpa.azurduy@gmail.com>
2022-08-22 12:47:34 -0400

    base section 5

---
+++ Section5_Fructus/Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Jorge Azurduy on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
