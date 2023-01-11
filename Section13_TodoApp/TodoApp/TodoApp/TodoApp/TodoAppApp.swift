e3632d2fa20c873f59646ec0f0e0701d244fb151
Jorge Azurduy <jpa.azurduy@gmail.com>
2023-01-10 23:49:54 -0400

    Todo App base

---
+++ Section13_TodoApp/TodoApp/TodoApp/TodoApp/TodoAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Jorge Azurduy on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
