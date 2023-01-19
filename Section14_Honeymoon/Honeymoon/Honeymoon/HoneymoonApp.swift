5112b8b062d1a455e95eb2141f8c47f095f5acfb
Jorge Azurduy <jpa.azurduy@gmail.com>
2023-01-19 12:05:16 -0400

    Honeymoon finished

---
+++ Section14_Honeymoon/Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Jorge Azurduy on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
